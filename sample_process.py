b544afb6c522fe66eb820e981bbfee9b3ca30729
Yuki Nagai <ytra.mail@gmail.com>
2023-04-16 10:29:16 +0900

    Renamed.

--- sample_process.py
+++ sample_process.py
@@ -1,3 +1,26 @@
+"""
+Script to generate a json files describing the MIT-Adobe FiveK Dataset \
+    items from the data downloaded and extracted from the archive.
+
+Usage:
+    python generate_dataset_json.py [-h] dir_root camera_models train_ratio val_ratio
+
+Arguments:
+    dir_root Path of the root directory where the tar file was extracted. \
+            The program generats `training.json`, `validation.json`, and `testing.json` files \
+            and stores in this directory.
+    camera_models  Path of the csv file listing a file id and its camera information.
+    train_ratio    Percentage of data used for `training` out of total data.
+    vaL_ratio    Percentage of data used for `validation` out of total data.
+
+Example:
+    python generate_dataset_json.py  ../data/MITAboveFiveK ../data/camera_models.csv 0.7 0.1
+
+Note:
+    Please see the official website for more information.
+    MIT-Adobe FiveK Dataset <https://data.csail.mit.edu/graphics/fivek/>
+"""
+
 import os
 import argparse
 from typing import List, Dict, Any

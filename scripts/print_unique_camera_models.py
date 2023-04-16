9c4e1d5c011f5d750c369d80adbc6027f338ad6f
Yuki Nagai <ytra.mail@gmail.com>
2023-04-16 19:00:02 +0900

    Formatted.

--- scripts/print_unique_camera_models.py
+++ scripts/print_unique_camera_models.py
@@ -26,7 +26,8 @@ import csv
 
 
 def main():
-    with open("../data/camera_models.csv", newline="", encoding="utf-8") as csvfile:
+    with open("./data/camera_models.csv", newline="",
+              encoding="utf-8") as csvfile:
         camera_models = set()
         reader = csv.DictReader(csvfile)
         for row in reader:
